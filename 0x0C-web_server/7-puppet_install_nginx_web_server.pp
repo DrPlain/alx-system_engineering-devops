5aee8bf86ef3dea7e400f0c16ff238b5e8355474
Gideon Obiasor <gideonoiasor@gmail.com>
2023-01-13 13:45:50 +0100

    install, configure, redirect and 404 with puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,28 +1,38 @@
-# install nginx, configure, redirect and customize 404 message with puppet
+# add stable version of nginx
+exec {'nginx stable repo':
+    command => 'sudo add-apt-repository ppa:nginx/stable',
+    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+    }
 
+# update packages
 exec {'apt update':
     command => 'sudo apt-get update',
     path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
     }
 
+# install nginx and ufw firewall
 package {'nginx': ensure    => 'installed'}
 package {'ufw': ensure      => 'installed'}
 
+# Allow nginx: full access
 exec {'grant ufw access to http':
     command => 'sudo ufw allow "Nginx Full"',
     path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
     }
 
+# create index file
 file {'/var/www/html/index.html':
     ensure  => 'present',
     content => 'Hello world!\n',
     }
 
+# create custom 404 error page file
 file {'/var/www/html/custom_404.html':
     ensure  => 'present',
     content => "Ceci n'est pas une page\n",
     }
 
+# configure serve and save in default file in sites-enabled
 file {'/etc/nginx/sites-enabled/default':
     ensure  => 'present',
     content => "server {
@@ -44,6 +54,7 @@ file {'/etc/nginx/sites-enabled/default':
                 }",
         }
 
+# restart nginx
 exec {'Restart nginx':
     command => 'service nginx restart',
     path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
